43914a11e9be135ef5a5f7f262e51836e9b59df6
BagasCaturS <bagascatursantoso@gmail.com>
2024-10-01 23:39:10 +0700

    Added dynamic functionality if location isnt found

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ const wind = document.getElementById("windSpeed");
 const humid = document.getElementById("humidity");
 const condition = document.getElementById("condition");
 const image = document.getElementById("weatherConditionImg");
+const error = document.getElementById("error");
+const main = document.getElementById("main");
 
+// saat user mengklik search akan menjalankan kode
+// dibawah
 search.addEventListener("click", async () => {
     const APIkey = "8013eb15ab60740a740ecf08e74ba7c0";
     const city = document.getElementById("location").value;
@@ -12,10 +16,29 @@ search.addEventListener("click", async () => {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`);
         const data = await response.json();
+        // apabila menembalikkan code 404 atau error
+        // maka akan menambahkan dan menghapuskan style seprti di code dibawah
+        if (data.cod == "404"){
+            main.classList.add("hidden");
+            error.classList.remove("hidden");
+            error.classList.add("block");   
+            return; 
+        }
+
+        //tambahain lg kelas-kelasnya biar muncul lagi
+        main.classList.remove("hidden");
+        main.classList.add("block");
+        error.classList.add("hidden");   
+
+        // memasukkan data yang telah di fetch
+        // kedalam elemen
         temperature.innerText = data.main.temp;
         wind.innerText = data.wind.speed;
         humid.innerText = data.main.humidity;
         condition.innerText = data.weather[0].description;
+
+        // mengubah foto2 sesaui dengan kondisi
+        // yang ada pada api
         switch (data.weather[0].main) {
             case 'Clear':
                 image.src = 'images/clear.png'
@@ -36,8 +59,12 @@ search.addEventListener("click", async () => {
             default:
                 image.src = 'images/WiCloudy.png';
         }
+        // melakukan parsing agar data yang berisi decimal
+        // di bulatkan
         temperature.innerHTML = `${parseInt(data.main.temp)}`;
         wind.innerHTML = `${parseInt(data.wind.speed)}`
+
+        // menangkap error untuk debugging
     } catch (error) {
         console.error(error);
     }
